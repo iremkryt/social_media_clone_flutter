170943839f3719d73e026c93e23cff02df4dc2f1
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-19 20:51:54 +0300

    Updated main

--- social_media_clone/lib/main.dart
+++ social_media_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_clone/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
