cdc3d0386353d23917375c0ba3cc529f88a06adf
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-19 20:51:41 +0300

    Updated login page

--- social_media_clone/lib/login_page.dart
+++ social_media_clone/lib/login_page.dart
@@ -14,21 +14,107 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: height * 0.25,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/waves.png"),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: height * 0.42,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/waves.png"),
+                  ),
                 ),
               ),
-            )
-          ],
+              customSizedBox(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Merhaba, \nHoşgeldin",
+                      style: TextStyle(
+                        fontSize: 30, 
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 37, 39, 55),
+                      ),
+                    ),
+                    customSizedBox(),
+                    TextField(
+                      decoration: customInputDecoration("Kullanıcı Adı"),
+                    ),
+                    TextField(
+                      decoration: customInputDecoration("Şifre"),
+                    ),
+                    customSizedBox(),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {}, 
+                        child: const Text("Şifremi Unuttum", 
+                          style: TextStyle(color: Color.fromARGB(255, 137, 54, 54)),
+                        ),
+                      ),
+                    ),
+                    //customSizedBox(),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Container(
+                          height: 50,
+                          width: 150,
+                          margin: const EdgeInsets.symmetric(horizontal: 60),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: const Color.fromARGB(255, 164, 211, 216),
+                          ),
+                          child: const Center(
+                            child: Text("Giriş Yap", 
+                              style: TextStyle(color: Color.fromARGB(255, 37, 39, 55)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                   // customSizedBox(),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {}, 
+                        child: const Text("Hesap Oluştur", 
+                          style: TextStyle(color: Color.fromARGB(255, 37, 39, 55)),
+                        ),
+                      ),
+                    ),
+                  ], 
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget customSizedBox() => const SizedBox(
+    height: 25,
+  );
+
+  InputDecoration customInputDecoration(String hintText) {
+    return InputDecoration(
+                    hintText: hintText,
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 37, 39, 55),
+                      ),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 37, 39, 55),
+                      ),
+                    ),
+                  );
   }
 }
