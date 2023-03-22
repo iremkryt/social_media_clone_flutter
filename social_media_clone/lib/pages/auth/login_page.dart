191facc7a9e991c31b660db0080700d5cc8693d2
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-22 12:21:06 +0300

    Update login page

--- social_media_clone/lib/pages/auth/login_page.dart
+++ social_media_clone/lib/pages/auth/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -10,6 +11,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  late String email, password;
+  final formKey = GlobalKey<FormState>();
+  final firebaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -32,52 +38,30 @@ class _LoginPageState extends State<LoginPage> {
               customSizedBox(),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Merhaba, \nHoşgeldin",
-                      style: TextStyle(
-                        fontSize: 30, 
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 37, 39, 55),
-                      ),
-                    ),
-                    customSizedBox(),
-                    kullaniciTextFile(),
-                    passwordTextFile(),
-                    customSizedBox(),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {}, 
-                        child: const Text("Şifremi Unuttum", 
-                          style: TextStyle(color: Color.fromARGB(255, 137, 54, 54)),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Merhaba, \nHoşgeldin",
+                        style: TextStyle(
+                          fontSize: 30, 
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 37, 39, 55),
                         ),
                       ),
-                    ),
-                    //customSizedBox(),
-                    Center(
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Container(
-                          height: 50,
-                          width: 150,
-                          margin: const EdgeInsets.symmetric(horizontal: 60),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: const Color.fromARGB(255, 164, 211, 216),
-                          ),
-                          child: const Center(
-                            child: Text("Giriş Yap", 
-                              style: TextStyle(color: Color.fromARGB(255, 37, 39, 55)),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                   // customSizedBox(),
-                    signUpButton(),
-                  ], 
+                      customSizedBox(),
+                      emailTextFile(),
+                      passwordTextFile(),
+                      customSizedBox(),
+                      forgotPasswordButton(),
+                      //customSizedBox(),
+                      signInButton(),
+                     // customSizedBox(),
+                      signUpButton(),
+                    ], 
+                  ),
                 ),
               )
             ],
@@ -93,21 +77,21 @@ class _LoginPageState extends State<LoginPage> {
 
   InputDecoration customInputDecoration(String hintText) {
     return InputDecoration(
-                    hintText: hintText,
-                    enabledBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 37, 39, 55),
-                      ),
-                    ),
-                    focusedBorder: const UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 37, 39, 55),
-                      ),
-                    ),
-                  );
+      hintText: hintText,
+      enabledBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Color.fromARGB(255, 37, 39, 55),
+        ),
+      ),
+      focusedBorder: const UnderlineInputBorder(
+        borderSide: BorderSide(
+          color: Color.fromARGB(255, 37, 39, 55),
+        ),
+      ),
+    );
   }
 
-    Center signUpButton(){
+  Center signUpButton(){
     return Center(
       child: TextButton(
         onPressed: () => Navigator.pushNamed(context, "/signUp"), 
@@ -119,16 +103,83 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-    TextFormField kullaniciTextFile(){
+  TextFormField emailTextFile(){
     return TextFormField(
-      decoration: customInputDecoration("Kullanıcı Adı"),
+      validator: (value) {
+        if (value!.isEmpty){
+          return "Bilgileri eksiksiz doldurunuz..";
+        } else {}
+      },
+      onSaved: (value) {
+        email = value!;
+      },
+      decoration: customInputDecoration("E-mail"),
     );
   }
 
   TextFormField passwordTextFile(){
     return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty){
+          return "Bilgileri eksiksiz doldurunuz..";
+        } else {}
+      },
+      onSaved: (value) {
+        password = value!;
+      },
       obscureText: true,
       decoration: customInputDecoration("Şifre"),
+    );
+  }
+
+  Center signInButton() {
+    return Center(
+      child: TextButton(
+        onPressed: signIn,
+        child: Container(
+          height: 50,
+          width: 150,
+          margin: const EdgeInsets.symmetric(horizontal: 60),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: const Color.fromARGB(255, 164, 211, 216),
+          ),
+          child: const Center(
+            child: Text("Giriş Yap", 
+              style: TextStyle(color: Color.fromARGB(255, 37, 39, 55)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void signIn() async {
+        if (formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          try {
+            final userResult = await firebaseAuth.signInWithEmailAndPassword(
+              email: email, 
+              password: password
+            );
+            Navigator.pushReplacementNamed(context,"/homePage");
+            print(userResult.user!.email);
+          } catch (e) {
+            print(e.toString());
+          }
+        } else {
+          
+        }
+      }
+
+  Center forgotPasswordButton(){
+    return Center(
+      child: TextButton(
+        onPressed: () {}, 
+        child: const Text("Şifremi Unuttum", 
+          style: TextStyle(color: Color.fromARGB(255, 137, 54, 54)),
+        ),
+      ),
     );
   }
 }
