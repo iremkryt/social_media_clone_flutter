6b7c5111582745a378db469f946736b465190254
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-23 10:38:21 +0300

    Created custom color

---
+++ social_media_clone/lib/utils/customColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const ScaffoldBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static const textButtonColor = Color.fromARGB(255, 37, 39, 55);
+  static const loginButtonTextColor = Color.fromARGB(255, 37, 39, 55);
+}
