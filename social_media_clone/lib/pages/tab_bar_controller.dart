5d30f042594eb60f4474fc8182c0d31a2d7bbd9e
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-03-28 10:45:02 +0300

    Updated tab bar controller

--- social_media_clone/lib/pages/tab_bar_controller.dart
+++ social_media_clone/lib/pages/tab_bar_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_clone/utils/customColors.dart';
+import 'package:social_media_clone/widgets/custom_app_bar.dart';
 
 class TabBarController extends StatefulWidget {
   const TabBarController({super.key});
@@ -10,20 +11,32 @@ class TabBarController extends StatefulWidget {
 }
 
 class _TabBarControllerState extends State<TabBarController> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: CustomColors.loginButtonTextColor,
-        items: [
-          _customBottomNavigationBarItem(CupertinoIcons.house, "Anasayfa"),
-          _customBottomNavigationBarItem(CupertinoIcons.search, "Ara"),
-          _customBottomNavigationBarItem(CupertinoIcons.photo_camera, "Reels"),
-          _customBottomNavigationBarItem(CupertinoIcons.shopping_cart, "Alışveriş"),
-          _customBottomNavigationBarItem(CupertinoIcons.person, "Profil"),
-        ],
-      ),
+      appBar: CustomAppBar(title: "Instagram Klon"),
+      bottomNavigationBar: customBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar customBottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (value) {
+        setState(() {
+          _currentIndex = value;
+        });
+      },
+      currentIndex: _currentIndex,
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: CustomColors.loginButtonTextColor,
+      items: [
+        _customBottomNavigationBarItem(CupertinoIcons.house, "Anasayfa"),
+        _customBottomNavigationBarItem(CupertinoIcons.search, "Ara"),
+        _customBottomNavigationBarItem(CupertinoIcons.photo_camera, "Reels"),
+        _customBottomNavigationBarItem(CupertinoIcons.shopping_cart, "Alışveriş"),
+        _customBottomNavigationBarItem(CupertinoIcons.person, "Profil"),
+      ],
     );
   }
 
